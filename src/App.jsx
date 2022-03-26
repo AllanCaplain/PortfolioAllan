1c2e509bced65f5bc0aff8d80fc094fe33c9c04c
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2022-03-26 20:02:16 +0100

    Making reset folder plus generals style in him

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Header from './components/component/header/Header';
+import Nav from './components/component/nav/Nav';
+import About from './components/component/about/About';
+import Experience from './components/component/experience/Experience';
+import Services from './components/component/services/Services';
+import Portfolio from './components/component/portfolio/Portfolio';
+import Testimonials from './components/component/testimonials/Testimonials';
+import Contact from './components/component/contact/Contact';
+import Footer from './components/component/footer/Footer';
 
 const App = () => {
   return (
     <>
-        
+       <Header />
+       <Nav />
+       <About />
+       <Experience />
+       <Services />
+       <Portfolio />
+       <Testimonials />
+       <Contact />
+       <Footer />
     </>
   )
 }
