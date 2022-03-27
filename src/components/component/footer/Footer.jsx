44caccc88c80687be0133dd27abc10bfbcd885f3
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2022-03-27 19:57:58 +0200

    Make footer component for desktop and mobile devices, last commit, project done :)

--- src/components/component/footer/Footer.jsx
+++ src/components/component/footer/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 function Footer() {
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+      <a href="#" className='footer_logo'>Allan Caplain</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.facebook.com/allan.caplain"><FaFacebookF className='footer_social-icon'/></a>
+        <a href="#"><FiInstagram className='footer_social-icon'/></a>
+        <a href="https://www.facebook.com/allan.caplain"><IoLogoTwitter className='footer_social-icon'/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Allan Caplain. All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
